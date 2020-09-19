29f0d02ab74f2956799df8b3049089ab610931e0
SANGCO <nsg1213@gmail.com>
2020-09-20 04:31:40 +0900

    요구사항 3 - @Test 애노테이션 메소드 실행

--- src/test/java/next/reflection/Junit4TestRunner.java
+++ src/test/java/next/reflection/Junit4TestRunner.java
@@ -2,10 +2,19 @@ package next.reflection;
 
 import org.junit.Test;
 
+import java.lang.reflect.Method;
+
 public class Junit4TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        Object o = clazz.getConstructor().newInstance();
+
+        for (Method method : clazz.getDeclaredMethods()) {
+            if (method.isAnnotationPresent(MyTest.class)) {
+                method.invoke(o);
+            }
+        }
+
     }
 }

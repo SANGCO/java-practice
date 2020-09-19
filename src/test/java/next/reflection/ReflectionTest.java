dd63293475c54b0c8258c308b33a76559dcb8bbc
SANGCO <nsg1213@gmail.com>
2020-09-20 03:36:40 +0900

    요구사항 1 - 클래스 정보 출력

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -1,6 +1,9 @@
 package next.reflection;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -9,23 +12,39 @@ import org.slf4j.LoggerFactory;
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
 
+
     @Test
     public void showClass() {
         Class<Question> clazz = Question.class;
         logger.debug(clazz.getName());
-    }
 
-    @Test
-    @SuppressWarnings("rawtypes")
-    public void constructor() throws Exception {
-        Class<Question> clazz = Question.class;
-        Constructor[] constructors = clazz.getConstructors();
-        for (Constructor constructor : constructors) {
-            Class[] parameterTypes = constructor.getParameterTypes();
-            logger.debug("paramer length : {}", parameterTypes.length);
-            for (Class paramType : parameterTypes) {
-                logger.debug("param type : {}", paramType);
+        logger.debug("----------------------constructor--------------------------");
+        for (Constructor<?> constructor : clazz.getDeclaredConstructors()) {
+            logger.debug(constructor.getName());
+            logger.debug(String.valueOf(constructor.getModifiers()));
+
+            for (Class<?> type : constructor.getParameterTypes()) {
+                logger.debug(type.getName());
             }
         }
+
+        logger.debug("----------------------field--------------------------");
+        for (Field field : clazz.getDeclaredFields()) {
+            logger.debug(field.getName());
+            logger.debug(String.valueOf(field.getModifiers()));
+        }
+
+        logger.debug("----------------------method--------------------------");
+        for (Method method : clazz.getDeclaredMethods()) {
+            logger.debug(method.getName());
+            logger.debug(String.valueOf(method.getModifiers()));
+
+            for (Class<?> type : method.getParameterTypes()) {
+                logger.debug(type.getName());
+            }
+        }
+
     }
+
+
 }

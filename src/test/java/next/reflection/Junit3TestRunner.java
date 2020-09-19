100b0a63d369816d57c334235a0e4820e6ab6805
SANGCO <nsg1213@gmail.com>
2020-09-20 04:16:45 +0900

    요구사항 2 - test로 시작하는 메소드 실행

--- src/test/java/next/reflection/Junit3TestRunner.java
+++ src/test/java/next/reflection/Junit3TestRunner.java
@@ -1,11 +1,25 @@
 package next.reflection;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 
 public class Junit3TestRunner {
+    private static final Logger logger = LoggerFactory.getLogger(Junit3TestRunner.class);
+
     @Test
     public void run() throws Exception {
-        Class<Junit3Test> clazz = Junit3Test.class;
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        Class<?> clazz = Junit3Test.class;
+        Object o = clazz.getConstructor().newInstance();
+
+        for (Method method : clazz.getDeclaredMethods()) {
+            if (method.getName().startsWith("test")) {
+                method.invoke(o);
+            }
+        }
+
     }
 }
--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -1,13 +1,12 @@
 package next.reflection;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.lang.reflect.Parameter;
-
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
